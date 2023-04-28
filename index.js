57e38b1e4c55417f0ea69b7b668dea37444cb987
Charlie <ttrcharlie@charlie-cloud.hu>
2023-04-28 10:50:21 +0200

    Set location to enviroment variable

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var http = require("http");
 require('dotenv').config();
-
+var url = process.env.URL;
 var port = process.env.PORT;
+
 http
   .createServer(function (req, res) {
-    res.writeHead(301, { Location: "https://example.com" });
+    res.writeHead(301, { Location: url });
     res.end();
   })
   .listen(port);
