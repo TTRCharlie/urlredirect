e7035020ca5b0d00135d37333575cae4aeebf357
Charlie <ttrcharlie@charlie-cloud.hu>
2023-06-12 20:30:07 +0200

    Add logging with DigitalOcean Apps PaaS platform support

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var http = require("http");
 require('dotenv').config();
 var url = process.env.URL;
-var port = process.env.PORT;
+var port = process.env.PORT || 8999;
 
 http
   .createServer(function (req, res) {
     res.writeHead(301, { Location: url });
-    console.info(`${req.socket.localAddress} ${res.statusCode} ${res.statusMessage};`)
+    console.info(`${req.headers['do-connecting-ip']} ${req.socket.remoteAddress} ${res.statusCode} ${res.statusMessage};`)
     res.end();
   })
   .listen(port);
